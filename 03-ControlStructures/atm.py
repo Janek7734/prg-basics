5ae1389ed0f15635ffcbb9b2ff48246ddd5c3850
Janek7734 <karakula.jan@gmail.com>
2025-11-07 10:00:49 +0100

    timer and atm files

--- 03-ControlStructures/atm.py
+++ 03-ControlStructures/atm.py
@@ -10,9 +10,11 @@ while True:
     print("1. Check balance")
     print("2. Deposit")
     print("3. Withdraw")
-    print("4. Exit")
+    print("4. Check PIN")
+    print("5. Change PIN")
+    print("6. Exit")
 
-    choice = input("Choose an option (1-4): ")
+    choice = input("Choose an option (1-6): ")
     print()
 
     if choice == '1':
@@ -29,6 +31,23 @@ while True:
         else:
             print("Insufficient balance.")
     elif choice == '4':
+        entered_pin = input("Enter your PIN: ")
+        if entered_pin == pin:
+            print("PIN is correct.")
+        else:
+            print("Incorrect PIN.")
+    elif choice == '5':
+        current_pin = input("Enter your current PIN: ")
+        if current_pin == pin:
+            new_pin = input("Enter your new 4-digit PIN: ")
+            if len(new_pin) == 4 and new_pin.isdigit():
+                pin = new_pin
+                print("PIN has been changed successfully.")
+            else:
+                print("Invalid PIN format. PIN must be 4 digits.")
+        else:
+            print("Incorrect current PIN.")
+    elif choice == '6':
         print("Exiting... Thank you for using the ATM!")
         break  # Exit the loop
     else:
